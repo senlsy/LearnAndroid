b9bad8b55664543feba0b6902d2e8f7ae7fca856
mark.lin <marklin@qeeka.com>
2017-05-22 10:29:56 +0800

    .

--- CodeSnippets/Chapter5/PA4AD_Ch05_Linkify/src/com/paad/linkify/MyActivty.java
+++ CodeSnippets/Chapter5/PA4AD_Ch05_Linkify/src/com/paad/linkify/MyActivty.java
@@ -1,8 +1,5 @@
 package com.paad.linkify;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,63 +11,69 @@ import android.text.util.Linkify.TransformFilter;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MyActivty extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		final TextView myTextView = (TextView) findViewById(R.id.text_view);
+        final TextView myTextView = (TextView) findViewById(R.id.text_view);
 
-		/**
-		 * Listing 5-7: Creating custom link strings in Linkify
-		 */
-		// Define the base URI.
-		String baseUri = "content://com.paad.earthquake/earthquakes/";
+        /**
+         * Listing 5-7: Creating custom link strings in Linkify
+         */
+        // Define the base URI.
+        String baseUri = "content://com.paad.earthquake/earthquakes/";
 
-		// Contruct an Intent to test if there is an Activity capable of
-		// viewing the content you are Linkifying. Use the Package Manager
-		// to perform the test.
-		PackageManager pm = getPackageManager();
-		Intent testIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(baseUri));
+        // Contruct an Intent to test if there is an Activity capable of
+        // viewing the content you are Linkifying. Use the Package Manager
+        // to perform the test.
+        PackageManager pm = getPackageManager();
+        Intent testIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(baseUri));
 
 //		boolean activityExists = testIntent.resolveActivity(pm) != null;
-		boolean activityExists = true;
+        boolean activityExists = true;
 
-		// If there is an Activity capable of viewing the content
-		// Linkify the text.
+        // If there is an Activity capable of viewing the content
+        // Linkify the text.
 //		if (activityExists) {
 //			int flags = Pattern.CASE_INSENSITIVE;
 //			Pattern p = Pattern.compile("\\bquake[\\s]?[0-9]+\\b", flags);
 //			Linkify.addLinks(myTextView, p, baseUri);
 //		}
 
-		// Uncomment to apply the match and transform filters
-		 if (activityExists) {
-		 int flags = Pattern.CASE_INSENSITIVE;
-		 Pattern p = Pattern.compile("\\bquake[\\s]?[0-9]+\\b", flags);
-		 Linkify.addLinks(myTextView, p, baseUri,new MyMatchFilter(), new MyTransformFilter());
-		 }
-	}
+        // Uncomment to apply the match and transform filters
+        if (activityExists) {
+            int flags = Pattern.CASE_INSENSITIVE;
+            Pattern p = Pattern.compile("\\bquake[\\s]?[0-9]+\\b", flags);
+            Linkify.addLinks(myTextView, p, baseUri, new MyMatchFilter(), new MyTransformFilter());
+            myTextView.setLinkTextColor(0xffffae00);
+        }
+    }
 
-	/**
-	 * Listing 5-8: Using a Linkify Match Filter
-	 */
-	class MyMatchFilter implements MatchFilter {
-		public boolean acceptMatch(CharSequence s, int start, int end) {
-			return (start == 0 || s.charAt(start - 1) != '!');
-		}
-	}
+    /**
+     * Listing 5-8: Using a Linkify Match Filter
+     */
+    class MyMatchFilter implements MatchFilter {
+        public boolean acceptMatch(CharSequence s, int start, int end) {
+            return (start == 0 || s.charAt(start - 1) != '!');
+        }
+    }
 
-	/**
-	 * Listing 5-9: Using a Linkify Transform Filter
-	 * 
-	 */
-	class MyTransformFilter implements TransformFilter {
-		public String transformUrl(Matcher match, String url) {
-			Log.e("lintest", "match"+match.group()+"  url="+url);
-			return url.toLowerCase().replace(" ", "");
-		}
-	}
+    /**
+     * Listing 5-9: Using a Linkify Transform Filter
+     */
+    class MyTransformFilter implements TransformFilter {
+
+        public String transformUrl(Matcher match, String url) {
+            Log.e("lintest", "match" + match.group() + "  url=" + url);
+            return url.toLowerCase().replace(" ", "");
+        }
+    }
 }
